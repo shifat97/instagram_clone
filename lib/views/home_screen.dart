216b948f02618362ae96fce877fd46389d12d6b3
shifat97 <shifatbinreza2@gmail.com>
2024-12-07 20:44:22 +0600

    add home screen post

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/views/home_screen_header.dart';
+import 'package:instagram_clone/views/home_screen_post.dart';
 import 'package:instagram_clone/views/home_screen_story.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,10 +11,12 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
           children: [
             HomeScreenHeader(),
             HomeScreenStory(),
+            HomeScreenPost(),
           ],
         ),
       ),
